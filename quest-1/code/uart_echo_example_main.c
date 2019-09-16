f7d355d1137352247156b298130293fb1538d455
Amy Dong <amydong@bu.edu>
2019-09-16 15:21:11 -0400

    ConsoleIOfinished

--- quest-1/code/uart_echo_example_main.c
+++ quest-1/code/uart_echo_example_main.c
@@ -1,11 +1,3 @@
-/* UART Echo Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include <string.h>
 #include "freertos/FreeRTOS.h"
@@ -14,53 +6,10 @@
 #include "driver/gpio.h"
 #include "esp_vfs_dev.h"
 
-/**
- * This is an example which echos any data it receives on UART1 back to the sender,
- * with hardware flow control turned off. It does not use UART driver event queue.
- *
- * - Port: UART1
- * - Receive (Rx) buffer: on
- * - Transmit (Tx) buffer: off
- * - Flow control: off
- * - Event queue: off
- * - Pin assignment: see defines below
- */
-
-#define ECHO_TEST_TXD  (UART_PIN_NO_CHANGE)
-#define ECHO_TEST_RXD  (UART_PIN_NO_CHANGE)
-#define ECHO_TEST_RTS  (UART_PIN_NO_CHANGE)
-#define ECHO_TEST_CTS  (UART_PIN_NO_CHANGE)
 
 #define BUF_SIZE (1024)
-
 #define LED 13
 
-static void echo_task(void *arg)
-{
-    /* Configure parameters of an UART driver,
-     * communication pins and install the driver */
-    uart_config_t uart_config = {
-        .baud_rate = 115200,
-        .data_bits = UART_DATA_8_BITS,
-        .parity    = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
-    };
-    uart_param_config(UART_NUM_0, &uart_config);
-    uart_set_pin(UART_NUM_0, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS);
-    //esp_vfs_dev_uart_use_driver(UART_NUM_0);
-    uart_driver_install(UART_NUM_0, BUF_SIZE * 2, 0, 0, NULL, 0);
-
-    // Configure a temporary buffer for the incoming data
-    uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
-
-    while (1) {
-        // Read data from the UART
-        int len = uart_read_bytes(UART_NUM_0, data, BUF_SIZE, 20 / portTICK_RATE_MS);
-        // Write data back to the UART
-        uart_write_bytes(UART_NUM_0, (const char *) data, len);
-    }
-}
 
 // function for counting length of buf
 int inputlength(char* buf) {
@@ -75,10 +24,9 @@ int inputlength(char* buf) {
     return charcount;
 }
 
+
 void app_main(void)
 {
-    //xTaskCreate(echo_task, "uart_echo_task", 1024, NULL, 10, NULL);
-
     ESP_ERROR_CHECK( uart_driver_install(UART_NUM_0, 256, 0, 0, NULL, 0) );
     esp_vfs_dev_uart_use_driver(UART_NUM_0);
 
@@ -122,7 +70,7 @@ void app_main(void)
                 charcount = inputlength(buf);
 
                 if (buf[0] == 's' && charcount == 1) {
-                    printf("Mode 3: some display dexadecimal stuff\n");
+                    printf("Mode 3: Dex to hex mode\n");
                     switchmodes = 3;
                 } else {
                     printf("echo: %s\n", buf);
@@ -130,14 +78,34 @@ void app_main(void)
                 vTaskDelay(50 / portTICK_RATE_MS);
                 break;
             case 3:
+                printf("Enter an integer: \n");
                 gets(buf);
-                if (buf[0] == 'u') {
-                    printf("executing mode 3 functions\n");
-                } else if (buf[0] == 's') {
-                    printf("Mode 1: Enter 't' to toggle on-board led on and off\n");
+                int charcountt, num;
+                charcountt = inputlength(buf);
+
+                if (buf[0] == 's' && charcountt == 1) {
+                    printf("Mode 1: LED toggle with 't'\n");
                     switchmodes = 1;
                 } else {
-                    printf("lalla\n");
+                    num = atoi(buf);
+                    char hexnum[100];
+                    int i = 0;
+                    long int remainder = 0;
+                    while(num != 0) {
+                        remainder = num % 16;
+                        if(remainder < 10) {
+                            remainder = remainder + 48;
+                        } else {
+                            remainder = remainder + 55;
+                        }
+                        hexnum[i++] = remainder;
+                        num = num / 16;
+                    }
+                    printf("Hex:");
+                    for(int j = i - 1; j >= 0; j--) {
+                        printf("%c", hexnum[j]);
+                    }
+                    printf("\n");
                 }
                 vTaskDelay(50 / portTICK_RATE_MS);
                 break;
