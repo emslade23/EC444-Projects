3de68774771a576e3783d48e54b2b81e377f2ff8
QuiannaM <mortquia@bu.edu>
2019-09-19 19:04:49 -0400

    Update q.c

--- quest-1/code/q.c
+++ quest-1/code/q.c
@@ -1,20 +1,18 @@
-/*
-  Adapted I2C example code to work with the Adafruit 14-segment Alphanumeric Display. Key notes: MSB!!
-  Emily Lam, Sept 2018, Updated Aug 2019
-*/
 #include <stdio.h>
-//#include <ctype.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include "driver/i2c.h"
-
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
 #include "driver/uart.h"
+#include "string.h"
 #include "driver/gpio.h"
-#include "esp_vfs.h"
 #include "esp_vfs_dev.h"
 
+//defines for display
 // 14-Segment Display
 #define SLAVE_ADDR                         0x70 // alphanumeric address
 #define OSC                                0x21 // oscillator cmd
@@ -36,18 +34,25 @@
 #define ACK_CHECK_DIS                      false// i2c master will not check ack
 #define ACK_VAL                            0x00 // i2c ack value
 #define NACK_VAL                           0xFF // i2c nack value
+/////////////////////////////////////////////////////
 
-//echo task
-#define ECHO_TEST_TXD  (UART_PIN_NO_CHANGE)
-#define ECHO_TEST_RXD  (UART_PIN_NO_CHANGE)
-#define ECHO_TEST_RTS  (UART_PIN_NO_CHANGE)
-#define ECHO_TEST_CTS  (UART_PIN_NO_CHANGE)
+uint16_t font_table(int num) {
+    uint16_t fonttable[9];
+    fonttable[0] = 0b0000110000111111; // 0
+    fonttable[1] = 0b0000000000000110; // 1
+    fonttable[2] = 0b0000000011011011; // 2
+    fonttable[3] = 0b0000000010001111; // 3
+    fonttable[4] = 0b0000000011100110; // 4
+    fonttable[5] = 0b0010000001101001; // 5
+    fonttable[6] = 0b0000000011111101; // 6
+    fonttable[7] = 0b0000000000000111; // 7
+    fonttable[8] = 0b0000000011111111; // 8
+    fonttable[9] = 0b0000000011101111; // 9
+    fonttable[10] = 0b0000000000000000; //
+    return fonttable[num];
+}
 
-#define BUF_SIZE 1024
-
-//uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
-
-// Function to initiate i2c -- note the MSB declaration!
+// init for display
 static void i2c_example_master_init(){
     // Debug
     printf("\n>> i2c Config\n");
@@ -69,8 +74,8 @@ static void i2c_example_master_init(){
 
     // Install I2C driver
     err = i2c_driver_install(i2c_master_port, conf.mode,
-                       I2C_EXAMPLE_MASTER_RX_BUF_DISABLE,
-                       I2C_EXAMPLE_MASTER_TX_BUF_DISABLE, 0);
+                             I2C_EXAMPLE_MASTER_RX_BUF_DISABLE,
+                             I2C_EXAMPLE_MASTER_TX_BUF_DISABLE, 0);
     // i2c_set_data_mode(i2c_master_port,I2C_DATA_MODE_LSB_FIRST,I2C_DATA_MODE_LSB_FIRST);
     if (err == ESP_OK) {printf("- initialized: yes\n\n");}
 
@@ -78,184 +83,195 @@ static void i2c_example_master_init(){
     i2c_set_data_mode(i2c_master_port, I2C_DATA_MODE_MSB_FIRST, I2C_DATA_MODE_MSB_FIRST);
 }
 
-// Utility  Functions //////////////////////////////////////////////////////////
-
-// Utility function to test for I2C device address -- not used in deploy
-int testConnection(uint8_t devAddr, int32_t timeout) {
-    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-    i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, (devAddr << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN);
-    i2c_master_stop(cmd);
-    int err = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
-    i2c_cmd_link_delete(cmd);
-    return err;
-}
-
-// Utility function to scan for i2c device
-static void i2c_scanner() {
-    int32_t scanTimeout = 1000;
-    printf("\n>> I2C scanning ..."  "\n");
-    uint8_t count = 0;
-    for (uint8_t i = 1; i < 127; i++) {
-        // printf("0x%X%s",i,"\n");
-        if (testConnection(i, scanTimeout) == ESP_OK) {
-            printf( "- Device found at address: 0x%X%s", i, "\n");
-            count++;
-        }
-    }
-    if (count == 0)
-        printf("- No I2C devices found!" "\n");
-    printf("\n");
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-// Alphanumeric Functions //////////////////////////////////////////////////////
-
 // Turn on oscillator for alpha display
 int alpha_oscillator() {
-  int ret;
-  i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-  i2c_master_start(cmd);
-  i2c_master_write_byte(cmd, ( SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
-  i2c_master_write_byte(cmd, OSC, ACK_CHECK_EN);
-  i2c_master_stop(cmd);
-  ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
-  i2c_cmd_link_delete(cmd);
-  vTaskDelay(200 / portTICK_RATE_MS);
-  return ret;
+    int ret;
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, ( SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, OSC, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    vTaskDelay(200 / portTICK_RATE_MS);
+    return ret;
 }
-
-// Set blink rate to off
+// Set blink rate to off for display
 int no_blink() {
-  int ret;
-  i2c_cmd_handle_t cmd2 = i2c_cmd_link_create();
-  i2c_master_start(cmd2);
-  i2c_master_write_byte(cmd2, ( SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
-  i2c_master_write_byte(cmd2, HT16K33_BLINK_CMD | HT16K33_BLINK_DISPLAYON | (HT16K33_BLINK_OFF << 1), ACK_CHECK_EN);
-  i2c_master_stop(cmd2);
-  ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd2, 1000 / portTICK_RATE_MS);
-  i2c_cmd_link_delete(cmd2);
-  vTaskDelay(200 / portTICK_RATE_MS);
-  return ret;
+    int ret;
+    i2c_cmd_handle_t cmd2 = i2c_cmd_link_create();
+    i2c_master_start(cmd2);
+    i2c_master_write_byte(cmd2, ( SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd2, HT16K33_BLINK_CMD | HT16K33_BLINK_DISPLAYON | (HT16K33_BLINK_OFF << 1), ACK_CHECK_EN);
+    i2c_master_stop(cmd2);
+    ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd2, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd2);
+    vTaskDelay(200 / portTICK_RATE_MS);
+    return ret;
 }
-
-// Set Brightness
+// Set Brightness for display
 int set_brightness_max(uint8_t val) {
-  int ret;
-  i2c_cmd_handle_t cmd3 = i2c_cmd_link_create();
-  i2c_master_start(cmd3);
-  i2c_master_write_byte(cmd3, ( SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
-  i2c_master_write_byte(cmd3, HT16K33_CMD_BRIGHTNESS | val, ACK_CHECK_EN);
-  i2c_master_stop(cmd3);
-  ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd3, 1000 / portTICK_RATE_MS);
-  i2c_cmd_link_delete(cmd3);
-  vTaskDelay(200 / portTICK_RATE_MS);
-  return ret;
+    int ret;
+    i2c_cmd_handle_t cmd3 = i2c_cmd_link_create();
+    i2c_master_start(cmd3);
+    i2c_master_write_byte(cmd3, ( SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd3, HT16K33_CMD_BRIGHTNESS | val, ACK_CHECK_EN);
+    i2c_master_stop(cmd3);
+    ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd3, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd3);
+    vTaskDelay(200 / portTICK_RATE_MS);
+    return ret;
 }
 
-
-////////////////////////////////////////////////////////////////////////////////
- bool isdigit(char str[4])
+bool isitdigit(char str[2])
 {
-  int i = 0;
-  for(i=0; i<4;i++){
-    if (str[i]>='0' && str[i]<='9'){
-      return true;
+    int i = 0;
+    int counter = 0;
+    for(i = 0; i < 2 ; i++) {
+        if (str[i] >= '0' && str[i] <= '9'){
+            counter++;
+        }
+    }
+    if(counter == 2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int min_increment(int minute, char min[2])
+{
+  if (minute == 59){
+    minute = 0;
+    min[0]= '0';
+    min[1]= '0';
+  }else{
+    minute++;
+    if(min[1]== '9'){
+      min[1]='0';
+      min[0]++;
+    }else{
+      min[1]++;
     }
   }
-  return false;
+  return minute;
 }
 
+int hr_increment(int min, int hr)
+{
+  if (min == 59){
+    hr++;
+  }
+  if (hr == 24){
+    hr = 1;
+  }
+  return hr;
+}
 
 
 static void test_alpha_display() {
-    // Debug
+    char hour[2];
+    char minute[2];
+    int int_hour = 0;
+    int int_minute = 0;
+    uint16_t displaybuffer[8];
+    int currenttime[] = {0,0,0,0};
     int ret;
-    printf(">> Test Alphanumeric Display: \n");
+        // Set up routines
+        // Turn on alpha oscillator
+        ret = alpha_oscillator();
+        ret = no_blink();
+        ret = set_brightness_max(0xF);
 
-    // Set up routines
-    // Turn on alpha oscillator
-    ret = alpha_oscillator();
-    if(ret == ESP_OK) {printf("- oscillator: ok \n");}
-    // Set display blink off
-    ret = no_blink();
-    if(ret == ESP_OK) {printf("- blink: off \n");}
-    ret = set_brightness_max(0xF);
-    if(ret == ESP_OK) {printf("- brightness: max \n");}
+    printf(">> Enter the hour, if one digit add 0 before it: \n");
+    gets(hour);
+    printf("%s\n", hour);
+    while(isitdigit(hour) == false || atoi(hour) > 24) {
+        if(isitdigit(hour) == false) {
+            printf("Error: Please enter only numbers:\n");
+            gets(hour);
+            printf("%s\n", hour);
+        } else if(atoi(hour) > 24) {
+            printf("Error: Please enter a number under 24:\n");
+            gets(hour);
+            printf("%s\n", hour);
+        }
+    }
+    printf("Hour is set to %s.\n", hour);
+    int_hour = atoi(hour);
 
-    // stuff for echo_task
-    uart_config_t uart_config = {
-        .baud_rate = 115200,
-        .data_bits = UART_DATA_8_BITS,
-        .parity    = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
-    };
-    uart_param_config(UART_NUM_0, &uart_config);
-    uart_set_pin(UART_NUM_0, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS);
-    ESP_ERROR_CHECK( uart_driver_install(UART_NUM_0, BUF_SIZE, 0, 0, NULL, 0));
+    printf(">> Enter the minute, if one digit add 0 before it: \n");
+    gets(minute);
+    printf("%s\n", minute);
+    while(isitdigit(minute) == false || atoi(minute) > 60) {
+        if(isitdigit(minute) == false) {
+            printf("Error: Please enter only numbers:\n");
+            gets(minute);
+            printf("%s\n", minute);
+        } else if(atoi(minute) > 60) {
+            printf("Error: Please enter a number under 60:\n");
+            gets(minute);
+            printf("%s\n", minute);
+        }
+    }
+    printf("Minute is set to %s.\n", minute);
+    int_minute = atoi(minute);
+
+    printf("Time is set to %d:%d.\n", int_hour, int_minute);
+
+
+  /* currenttime[0] = hour[0] - '0';
+    currenttime[1] = hour[1] - '0';
+    currenttime[2] = minute[0] - '0';
+    currenttime[3] = minute[1] - '0';
+
+    for (int i = 0; i <= 3; i++) {
+        displaybuffer[i] = font_table(currenttime[i]);
+    }*/
+
+    while(1) {
+      //start of q's change
+      /*int_minute = atoi(minute);
+      int_hour = atoi(hour);*/
+
+      currenttime[0] = hour[0] - '0';
+      currenttime[1] = hour[1] - '0';
+      currenttime[2] = minute[0] - '0';
+      currenttime[3] = minute[1] - '0';
+
+      for (int i = 0; i <= 3; i++) {
+          displaybuffer[i] = font_table(currenttime[i]);
+      }
+
+      int_minute = min_increment(int_minute, minute);
+      int_hour = hr_increment(int_minute, int_hour);
+
+    //end of q's change
+        // Send commands characters to display over I2C
+        i2c_cmd_handle_t cmd4 = i2c_cmd_link_create();
+        i2c_master_start(cmd4);
+        i2c_master_write_byte(cmd4, ( SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
+        i2c_master_write_byte(cmd4, (uint8_t)0x00, ACK_CHECK_EN);
+        for (uint8_t i=0; i<8; i++) {
+            i2c_master_write_byte(cmd4, displaybuffer[i] & 0xFF, ACK_CHECK_EN);
+            i2c_master_write_byte(cmd4, displaybuffer[i] >> 8, ACK_CHECK_EN);
+        }
+        i2c_master_stop(cmd4);
+        ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd4, 1000 / portTICK_RATE_MS);
+        i2c_cmd_link_delete(cmd4);
+
+          vTaskDelay(800 / portTICK_RATE_MS);
+        //if(ret == ESP_OK) {
+        //    printf("I displayed this!\n\n");
+        //}
+    }
+}
+
+void app_main(void)
+{
+    ESP_ERROR_CHECK( uart_driver_install(UART_NUM_0, 256, 0, 0, NULL, 0) );
     esp_vfs_dev_uart_use_driver(UART_NUM_0);
 
-    // Configure a temporary buffer for the incoming data
-    //uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
-
-
-    char buffer[4];
-
-   while (1) {
-      // ask for user input and print to screen
-      printf("Enter current time:\n");
-      //int len = uart_read_bytes(UART_NUM_0, data, BUF_SIZE, 20 / portTICK_RATE_MS);
-      gets(buffer);
-
-      while(!isdigit(buffer)){
-        printf("Error: need to input a number:\n");
-        gets(buffer);
-        printf("%s", buffer);
-
-      }
-      // Write data back to the UART
-
-      //printf("Hex: %s \n", buffer); //0x%x \n", data);
-
-
-
-
-    //  (UART_NUM_0, (const char *) data, len);
-
-      uint16_t displaybuffer[8];
-      displaybuffer[0] = 0b0101001000000001;  // T.
-      displaybuffer[1] = 0b0101001000001111;  // D.
-      displaybuffer[2] = 0b0100000000111001;  // C.
-      displaybuffer[3] = 0b0100000000111000;  // L.
-
-
-      // Send commands characters to display over I2C
-      i2c_cmd_handle_t cmd4 = i2c_cmd_link_create();
-      i2c_master_start(cmd4);
-      i2c_master_write_byte(cmd4, ( SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
-      i2c_master_write_byte(cmd4, (uint8_t)0x00, ACK_CHECK_EN);
-      for (uint8_t i=0; i<8; i++) {
-        i2c_master_write_byte(cmd4, displaybuffer[i] & 0xFF, ACK_CHECK_EN);
-        i2c_master_write_byte(cmd4, displaybuffer[i] >> 8, ACK_CHECK_EN);
-      }
-      i2c_master_stop(cmd4);
-      ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd4, 1000 / portTICK_RATE_MS);
-      i2c_cmd_link_delete(cmd4);
-
-      // for (int i = 0; i < 8; i++) {
-      //     printf("%04x\n", displaybuffer[i]);
-      // }
-      }
-    }
-
-
-//}
-
-void app_main() {
     i2c_example_master_init();
-    i2c_scanner();
-
-    xTaskCreate(test_alpha_display,"test_alpha_display", 4096, NULL, 5, NULL);
+    xTaskCreate(test_alpha_display,"test_alpha_display", 4096, NULL, configMAX_PRIORITIES, NULL);
 }
