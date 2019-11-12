58c81bbc313439542e58e709923a12f462c4ba8b
Elizabeth Slade <35977934+emslade23@users.noreply.github.com>
2019-11-12 13:58:24 -0500

    Create tfmini.c

---
+++ quest-5/code/tfmini.c
@@ -0,0 +1,77 @@
+/* UART Echo Example
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/uart.h"
+#include "driver/gpio.h"
+
+/**
+ * This is an example which echos any data it receives on UART1 back to the sender,
+ * with hardware flow control turned off. It does not use UART driver event queue.
+ *
+ * - Port: UART1
+ * - Receive (Rx) buffer: on
+ * - Transmit (Tx) buffer: off
+ * - Flow control: off
+ * - Event queue: off
+ * - Pin assignment: see defines below
+ */
+
+#define ECHO_TEST_TXD  17
+#define ECHO_TEST_RXD  16
+#define ECHO_TEST_RTS  (UART_PIN_NO_CHANGE)
+#define ECHO_TEST_CTS  (UART_PIN_NO_CHANGE)
+
+#define BUF_SIZE (2400)//1024
+
+static void echo_task(void *arg)
+{
+    /* Configure parameters of an UART driver,
+     * communication pins and install the driver */
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
+    };
+    uart_param_config(UART_NUM_1, &uart_config);
+    uart_set_pin(UART_NUM_1, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS);
+    uart_driver_install(UART_NUM_1, BUF_SIZE * 2, 0, 0, NULL, 0);
+
+    // Configure a temporary buffer for the incoming data
+    uint8_t *data = (uint8_t *) malloc(20);
+    uint16_t val = 0;
+    while (1) {
+        // Read data from the UART
+        int len = uart_read_bytes(UART_NUM_1, data, 20, 20 / portTICK_RATE_MS);
+       //printf("length: %d ", len);
+        if (data[0] == 0x59 && data[1] == 0x59)
+        {
+            val = (data[3] << 8) | (data[2]);
+            printf("distance %d cm\n", val);
+        }
+    //    printf("%d", data[0]);
+    //     printf("0x%x ", data[0]);
+    //     printf("0x%x ", data[1]);
+    //     printf("0x%x ", data[2]);
+    //     printf("0x%x ", data[3]);
+    //     printf("0x%x ", data[4]);
+    //    printf("0x%x ", data[5]);
+    //    printf("0x%x ", data[6]);
+    //    printf("0x%x ", data[7]);
+    //    printf("0x%x \n", data[8]);
+    //    vTaskDelay(500 / portTICK_PERIOD_MS);
+        //uart_write_bytes(UART_NUM_1, (const char *) data, len);
+    }
+}
+
+void app_main(void)
+{
+    xTaskCreate(echo_task, "uart_echo_task", 1024, NULL, 10, NULL);
+}
