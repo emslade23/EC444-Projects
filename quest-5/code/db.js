10075281c4925fc637cd31309d3a0e26cac5f66c
Elizabeth Slade <35977934+emslade23@users.noreply.github.com>
2019-11-22 22:20:34 -0500

    Update db.js

--- quest-5/code/db.js
+++ quest-5/code/db.js
@@ -1,10 +1,14 @@
 // Author: Amy Dong, Elizabeth Slade, Quianna Mortimer
 // Date: 11/22/2019
 
+// import libraries
 var level = require('level');
 var path = require('path');
+// create a database folder to store data
 var dbPath = path.join(__dirname, 'mydb');
+// store values that are jsons
 var db = level(dbPath, {
     valueEncoding: 'json'
 });
+// export database
 module.exports = db;
